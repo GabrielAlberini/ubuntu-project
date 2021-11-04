d0569e501e5a4629b8764f2df604ab878cbf90e8
Nicolás Setzes <nicosetzes@gmail.com>
2021-11-04 11:55:51 -0300

    V1.6 I've added the necessary hamburger menu and the carousel. Worked on some styles.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,42 @@
+// Remove the "preload" class from the body as page starts the load.
+
+const body = document.getElementById("body");
+
+window.addEventListener("load", () => {
+  setInterval(() => {
+    console.log(body);
+    body.classList.remove("preload");
+  }, 1);
+});
+
+// Hamburger menu
+
 const hamburger = document.getElementById("hamburger");
 
-const nav__list = document.getElementById("nav__list");
-
-console.log(hamburger, nav__list);
+const nav = document.getElementById("nav");
 
 hamburger.addEventListener("click", () => {
-  nav__list.classList.toggle("show");
+  nav.classList.toggle("show");
 });
 
-// Removiendo la clase show al superar el breakpoint de 768
+// Removing the "show" class when going beyond 768px wide (viewport)
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth >= 768) nav__list.classList.remove("show");
+  if (window.innerWidth >= 992) nav.classList.remove("show");
 });
 
-// Smooth scrolling
+// Eliminating all animations when resizing
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+// Smooth scrolling (header)
 
 const terapia = document.getElementById("terapia");
 
@@ -24,10 +46,86 @@ terapia.addEventListener("click", () => {
     .scrollIntoView({ block: "start", behavior: "smooth" });
 });
 
-const vouchers = document.getElementById("voucher");
+const voucher = document.getElementById("voucher");
 
-vouchers.addEventListener("click", () => {
+voucher.addEventListener("click", () => {
   document
     .getElementById("section__vouchers")
     .scrollIntoView({ block: "start", behavior: "smooth" });
 });
+
+const meditaciones = document.getElementById("meditaciones");
+
+// meditaciones.addEventListener("click", () => {
+//   document
+//     .getElementById("")
+//     .scrollIntoView({ block: "start", behavior: "smooth" });
+// });
+
+const sobreMi = document.getElementById("sobreMi");
+
+// sobreMi.addEventListener("click", () => {
+//   document
+//     .getElementById("")
+//     .scrollIntoView({ block: "start", behavior: "smooth" });
+// });
+
+const contacto = document.getElementById("contacto");
+
+// contacto.addEventListener("click", () => {
+//   document
+//     .getElementById("")
+//     .scrollIntoView({ block: "start", behavior: "smooth" });
+// });
+
+// Smooth scrolling (buttons)
+
+// Carousel
+
+let slidePosition = 0;
+const slides = document.getElementsByClassName("carousel__item");
+const totalSlides = slides.length;
+
+document
+  .getElementById("carousel__button--next")
+  .addEventListener("click", function () {
+    moveToNextSlide();
+  });
+document
+  .getElementById("carousel__button--prev")
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel__item--visible");
+    slide.classList.add("carousel__item--hidden");
+  }
+
+  slides[slidePosition].classList.add("carousel__item--visible");
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
+
+// Carousel auto-play
+
+setInterval(moveToNextSlide, 7500);
